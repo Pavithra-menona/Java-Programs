b7054da21f21c0c29eedf325fe96c9bad58b6e6c
Pavithra-menona <pavithramenona@gmail.com>
2025-07-22 07:46:12 +0530

    Create evennos.java

---
+++ evennos.java
@@ -0,0 +1,19 @@
+package module1lab;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		
+		/*Author:Pavithra Menon A
+		 * Date:17/07/2025
+		 */
+		
+		System.out.println("Even numbers between 1 and 100");
+		for(int i=2;i<=100;i+=2)
+		{
+			System.out.println(i+" ");
+		}
+
+	}
+
+}
